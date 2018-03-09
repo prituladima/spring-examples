be8abe70890e799e370b02633603146591313b1a
prituladima <prituladima@gmail.com>
2018-03-09 02:39:36 +0200

    ---

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,10 +10,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main{
 
     public static void main(String[] args) throws InterruptedException{
-        // Creating a Context Application object by reading
-        // the configuration of the 'AppConfiguration' class.
+
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class);
-//
+
         System.out.println("----------");
         Language language = (Language) context.getBean("getVietnameseLanguage");
 
@@ -21,23 +20,14 @@ public class Main{
         System.out.println("Call language.sayBye(): "+ language.getBye());
 
         System.out.println("----------");
-//
-//        System.out.println("----------");
-//        Language language1 = (Language) context.getBean("language-English");
-//
-//        System.out.println("Bean Language: "+ language1);
-//        System.out.println("Call language.sayBye(): "+ language1.getBye());
-//
-//        System.out.println("----------");
-//
-//
+
         GreetingService service = (GreetingService) context.getBean("greetingService");
 
 
         service.sayGreeting();
-//
+
         System.out.println("----------");
-//
+
         MyComponent myComponent = (MyComponent) context.getBean("myComponent");
 
         myComponent.showAppInfo();
@@ -52,8 +42,6 @@ public class Main{
             RarelyUsedBean rarelyUsedBean = context1.getBean(RarelyUsedBean.class);
             System.out.println(System.identityHashCode(rarelyUsedBean));
         }
-
-
 
     }
 
